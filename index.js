3f468ff1ff58a96839fb230b8e61400f5225df40
Parvez-hasan <parvez7788@gmail.com>
2025-11-10 23:31:06 +0600

    get, update and detele methord added.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 4000;
 
@@ -29,6 +29,53 @@ app.get('/', (req, res) => {
 async function run () {
     try{
         await client.connect();
+        const db = client.db('homehero_db')
+        const serviceCollection = db.collection('services');
+
+        // all services get
+        app.get('/services', async (req, res) => {
+          const cursor = serviceCollection.find();
+          const result = await cursor.toArray();
+          res.send(result)
+        })
+
+        // single service get
+        app.get('/services/:id', async (req, res)=>{
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id)}
+          const result = await serviceCollection.findOne(query);
+          res.send(result)
+        })
+
+        // service add 
+        app.post('/services', async(req,res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        })
+
+        //update service
+        app.patch('/services/:id' , async(req,res) => {
+          const id = req.params.id;
+          const updateService = req.body;
+          const query = { _id: new ObjectId(id)}
+          const update = {
+            $set: updateService
+          }
+          const result = await serviceCollection.updateOne(query, update);
+          res.send(result);
+        })
+
+        //delete 
+        app.delete('/services/:id', async(req,res) => {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id)}
+          const result = await serviceCollection.deleteOne(query);
+          res.send(result);
+        })
+
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
