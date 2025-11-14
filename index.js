2f8a64962ca386a2717b8130e41242227dc50e49
Parvez-hasan <parvez7788@gmail.com>
2025-11-14 23:28:55 +0600

    same edit the site

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 4000;
+
+console.log(process.env);
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-// name : homeherodb
-//pass : s0HRApv48ShWUJub
 
-const uri = "mongodb+srv://homeherodb:s0HRApv48ShWUJub@project-1.zd08b5r.mongodb.net/?appName=project-1";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@project-1.zd08b5r.mongodb.net/?appName=project-1`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -31,10 +32,38 @@ async function run () {
         await client.connect();
         const db = client.db('homehero_db')
         const serviceCollection = db.collection('services');
+        const usersCollection = db.collection('users')
+
+        // users
+        // app.post('/users', async(req, res ) => {
+        //   const 
+        // })
 
         // all services get
         app.get('/services', async (req, res) => {
-          const cursor = serviceCollection.find();
+          
+          console.log(req.query);
+          const email = req.query.email;
+          const query = {}
+          if(email) {
+            query.email = email;
+          }
+
+          const cursor = serviceCollection.find(query);
+          const result = await cursor.toArray();
+          res.send(result)
+        })
+           // home six service
+        app.get('/services-home', async (req, res) => {
+          
+          console.log(req.query);
+          const email = req.query.email;
+          const query = {}
+          if(email) {
+            query.email = email;
+          }
+
+          const cursor = serviceCollection.find(query).limit(6);
           const result = await cursor.toArray();
           res.send(result)
         })
@@ -46,6 +75,28 @@ async function run () {
           const result = await serviceCollection.findOne(query);
           res.send(result)
         })
+
+        // update service 
+        app.get("/services/:id", async (req, res) => {
+        const id = req.params.id;
+        const result = await serviceCollection.findOne({ _id: new ObjectId(id) });
+        res.send(result);
+        });
+
+        app.put("/services/:id", async (req, res) => {
+        const id = req.params.id;
+        const body = req.body;
+
+        const filter = { _id: new ObjectId(id) };
+         const update = {
+         $set: body,
+         };
+
+        const result = await serviceCollection.updateOne(filter, update);
+         res.send(result);
+          });
+
+
 
         // service add 
         app.post('/services', async(req,res) => {
@@ -66,14 +117,13 @@ async function run () {
           res.send(result);
         })
 
-        //delete 
+        //delete service
         app.delete('/services/:id', async(req,res) => {
           const id = req.params.id;
           const query = { _id: new ObjectId(id)}
           const result = await serviceCollection.deleteOne(query);
           res.send(result);
         })
-
 
 
         await client.db("admin").command({ ping: 1 });
